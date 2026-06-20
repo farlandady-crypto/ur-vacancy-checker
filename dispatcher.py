5738135d91bcef816ef6a885b56160f219503596
farlandady-crypto <farlandady@gmail.com>
2026-06-20 15:59:27 +0800

    Create dispatcher.py

---
+++ dispatcher.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+TOKEN = os.environ["GH_TOKEN"]
+REPO = os.environ["GH_REPO"]
+
+API = f"https://api.github.com/repos/{REPO}/dispatches"
+
+
+def dispatch(event_type, payload):
+    requests.post(
+        API,
+        json={
+            "event_type": event_type,
+            "client_payload": payload
+        },
+        headers={
+            "Authorization": f"Bearer {TOKEN}",
+            "Accept": "application/vnd.github+json"
+        }
+    )
+
+
+# 示例
+# dispatch("pause", {})
+# dispatch("resume", {})
